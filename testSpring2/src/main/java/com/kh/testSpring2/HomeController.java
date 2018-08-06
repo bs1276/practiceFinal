7781444b91f6ae94656aab4626c5525954563ba2
user1 <user1@user1-PC>
2018-08-06 13:34:32 +0900

    TEST

--- testSpring2/src/main/java/com/kh/testSpring2/HomeController.java
+++ testSpring2/src/main/java/com/kh/testSpring2/HomeController.java
@@ -25,15 +25,12 @@ public class HomeController {
 	@RequestMapping(value = "/index.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		//ETWETWETAWETAWETDFASDWETTTTTTTTTFDDDDDDDDDDDDDDDDDDDDDDDDDDDD
-		//555555
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		//TESTTESTTWETQWETQWETWQETSADFASD
 		return "home";
 	}
 	
